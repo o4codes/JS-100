0b0b5ba55b5a4d4ef1b50ab685eff98c04ad0836
shadrach <oforkansi.shadrach@gmail.com>
2021-11-06 10:53:59 +0100

    2nd day

---
+++ silly_story_generator(2)/main.js
@@ -0,0 +1,53 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+    const random = Math.floor(Math.random()*array.length);
+    return array[random];
+  }
+
+
+let storytext = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+let insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+let inserty = ["the soup kitchen", "Disneyland", "the White House"];
+let insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+    let newStory = storytext;
+    let xItem = randomValueFromArray(insertx);
+    let yItem = randomValueFromArray(inserty);
+    let zItem = randomValueFromArray(insertz);
+
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':inserty:', yItem);
+    newStory = newStory.replace(':insertz:', zItem);
+
+    if (customName.value !== '') {
+        let name = customName.value;
+        newStory = newStory.replace('Bob', name);
+    }
+
+    if (document.getElementById('uk').checked) {
+
+        let weight = Math.round(300/14) + ' stone';
+        let temperature =  Math.round(((94 - 32) * 5) / 9) +  ' centigrade';
+
+        newStory = newStory.replace('94', temperature);
+        newStory = newStory.replace('300', weight);   
+    }
+
+    if (document.getElementById('us').checked) {
+        let us = document.getElementById('us').value;
+        newStory = newStory.replace(':inserty:', us);
+    }
+
+   
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
